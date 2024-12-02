6a4f0a27b691efd9b6b205e731a946983799532d
Agathe Makumbi <makagathe7@gmail.com>
2024-12-02 12:14:03 +0100

    ajout des roles pour chaque dev dans le fichier I_ApiCRUD

--- src/CRUDManager/I_ApiCRUD.php
+++ src/CRUDManager/I_ApiCRUD.php
@@ -22,7 +22,7 @@ interface I_ApiCRUD
     //public function supprimeTable():bool;
     public function existePersonne(string $noTel, string $email): bool;
 
-    //methodes User
+    //methodes User - Agathe
     public function signUp(string $username, string $email, string $password): bool;
     public function login(string $email, string $password): bool;
     public function createUser(User $user): bool;
@@ -31,35 +31,35 @@ interface I_ApiCRUD
     public function verifyUser(string $email, string $token, bool $isVerified): bool;
     public function blockUser(int $id, $isBlocked): bool;
 
-    //methodes Post
+    //methodes Post - Michael
     public function createPost(Post $post): bool;
     public function showPosts(): array;
     public function updatePost(Post $post): bool;
     public function deletePost(int $id): bool;
 
-    //methodes Commentaire
+    //methodes Commentaire - Michael
     public function createComment(Comment $comment): bool;
     public function showComments(): array;
     public function updateComment(Comment $comment): bool;
     public function deleteComment(int $id): bool;
 
-    //methodes Like
+    //methodes Like - Michael
     public function createLike(Like $like): bool;
     public function deleteLike(int $id): bool;
 
-    //methodes Category
+    //methodes Category - Joanah
     public function createCategory(Category $category): bool;
     public function showCategories(): string;
     public function updateCategory(Category $category): bool;
     public function deleteCategory(int $id): bool;
 
-    // methodes City
+    // methodes City - Michael
     public function createCity(City $city): bool;
     public function showCities(): array;
     public function updateCity(City $city): bool;
     public function deleteCity(int $id): bool;
 
-    //methodes media 
+    //methodes media - Agathe
     public function uploadMedia(Media $media): bool;
     public function deleteMedia(int $id): bool;
 }
