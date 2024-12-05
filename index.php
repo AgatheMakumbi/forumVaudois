345cc7dc3dd4e76982cadce5ef3044e841e08088
Agathe Makumbi <makagathe7@gmail.com>
2024-12-05 12:15:03 +0100

    Installation de Symphony Mailer et test de Mailhog sur la page index.php TOUT fonctionne

--- index.php
+++ index.php
@@ -13,9 +13,39 @@ echo $db;
 echo "<br>";
 echo $db->showCategories();
 
-$dbPath = '/Applications/MAMP/htdocs/ForumVaudois/db/forumvaudois.db';
-if (!file_exists($dbPath)) {
-    die("Database file not found: " . $dbPath);
+
+// Ce script permet d'envoyer un mail sur le serveur mail : MailHog en local
+/* Maintenant que tout est prêt, il faut lancer le serveur de messagerie mailhog
+Démarrage du serveur de messagerie
+Windows :
+Lancer l'exécution du fichier : MailHog_windows_386.exe
+MacOs :
+En ligne de commande (terminal)
+// Remarque : Pour que cela fonctionne, il faut avoir démarré le serveur ;-) */
+// Libraire permettant l'envoi de mail (Symfony Mailer)
+
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\Email;
+
+$transport = Transport::fromDsn('smtp://localhost:1025');
+$mailer = new Mailer($transport);
+$email = (new Email())
+    ->from('dominique.martin@heig-vd.ch')
+    ->to('desti.nataire@quelquepart.com')
+    //->cc('cc@exemple.com')
+    //->bcc('bcc@exemple.com')
+    //->replyTo('replyto@exemple.com')
+    //->priority(Email::PRIORITY_HIGH)
+    ->subject('Concerne : Envoi de mail')
+    ->text('Un peu de texte')
+    ->html('<h1>Un peu de html</h1>');
+$result = $mailer->send($email);
+if ($result == null) {
+    echo "Un mail a été envoyé ! <a href='http://localhost:8025'>voir le
+mail</a>";
+} else {
+    echo "Un problème lors de l'envoi du mail est survenu";
 }
 
 ?>
@@ -30,12 +60,12 @@ if (!file_exists($dbPath)) {
 </head>
 
 <body>
-    <?php include './components/header.php'?>
+    <?php include './components/header.php' ?>
 
     <main class="main-content">
         <h1 class="welcome-text">Bienvenue sur Secret News</h1>
         <p class="subtitle">Découvrez notre plateforme et rejoignez notre communauté grandissante. Connectez-vous ou inscrivez-vous pour accéder à tout le contenu exclusif.</p>
-        
+
     </main>
 </body>
 
