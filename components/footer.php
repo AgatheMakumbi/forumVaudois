cb81634c22287ff23471dfa8c93bcf251df94287
JoshuaAbes <joshua.abessolo@heig-vd.ch>
2024-12-18 16:23:55 +0100

    Correction du footer ajout des style dans style.css

---
+++ components/footer.php
@@ -0,0 +1,23 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h4>Information</h4>
+                <ul>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Support</a></li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <h4>Emplacement</h4>
+                <p>Adresse : Av. des Sports 20, 1401 Yverdon-les-Bains</p>
+                <p>Téléphone : 024 557 76 00</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <p>&copy; 2024 HEIG-VD Progserv2</p>
+            </div>
+        </div>
+    </div>
+</footer>
