8857ff607b9a4d6f766d65b4cd080a251f05a486
Math-ilde0 <mathilde.jaccard@hes-so.ch>
2024-12-22 11:49:44 +0100

    Amélioration du style du footer (suppression des liens inutiles, agrandissement du titre Contact, réduction de la largeur), ajustement du header (ombre, suppression des cases, redimensionnement du logo et du header), et corrections globales de style.

--- components/footer.php
+++ components/footer.php
@@ -1,18 +1,13 @@
 <footer>
     <div class="footer-content">
-        <div class="info">
-            <h3>Liens Utiles</h3>
-            <ul>
-                <li><a href="/ForumVaudois/pages/news.php">Voir les Posts</a></li>
-                <li><a href="/ForumVaudois/pages/createPost.php">Créer un Post</a></li>
-                <li><a href="/ForumVaudois/pages/about.php">À Propos</a></li>
-            </ul>
-        </div>
         <div class="company">
             <h3>Contact</h3>
             <p>Adresse : Av. des Sports 20, 1401 Yverdon-les-Bains</p>
             <p>Téléphone : <a href="tel:+41245577600">024 557 76 00</a></p>
             <p>Email : <a href="mailto:contact@forumvaudois.ch">contact@forumvaudois.ch</a></p>
+            <br>
+            <br><br> <br> <br><br><br><br> <br> <br><br> <br>
+            <p>&copy; 2024 Forum Vaudois - HEIG-VD ProgServ2</p>
         </div>
         <div class="contact-form">
             <form action="#" method="post">
@@ -28,8 +23,5 @@
                 <button type="submit">Envoyer</button>
             </form>
         </div>
-    </div>
-    <div class="footer-bottom">
-        <p>&copy; 2024 Forum Vaudois - HEIG-VD ProgServ2</p>
     </div>
 </footer>
--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $db = new DbManagerCRUD();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/style.css?v=<?= time(); ?>">
+    <link rel="stylesheet" href="./assets/css/style.css?v=1">
     <title>Forum Vaudois - Accueil</title>
 </head>
 
